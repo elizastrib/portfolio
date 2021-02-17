cfe661a61c879b9ed0b87f3fea4a1df4be2a2abf
Eliza Stribling <elizastribling@gmail.com>
2021-02-18 09:45:22 +1100

    Add JS interactions and Contentful

--- scripts/modals.js
+++ scripts/modals.js
@@ -1,35 +1,59 @@
+const body = document.querySelector('body')
 
+const projects = document.querySelectorAll('div.project')
 
-const projectList = document.querySelector('section#projects-list')
-const projects = projectList.querySelectorAll('div.project')
-
-
-const modal = document.querySelector('div#modal')
+const modals = document.querySelectorAll('div#modal')
 const modalBG = document.querySelector('div#modal-bg')
+const modalLinks = modal.querySelectorAll('a')
+
+const openModal = document.querySelector('div#modal.show')
 
 const closeLinks = document.querySelectorAll('div.close a')
 
 
+const modalFunctionality = function () {
 
+    projects.forEach(project => {
 
-projects.forEach(project => {
-    project.addEventListener('click', function(event){
-        modalBG.classList.toggle('show')
-        modal.classList.toggle('show')
-        console.log('hello')
+        project.addEventListener('click', function(event){
+    
+            body.style.backgroundColor = project.dataset.color
+            closeLinks.forEach(link => {
+                link.style.color = project.dataset.color
+            })
+            modalLinks.forEach(link => {
+                link.style.color = project.dataset.color
+            })
+    
+            modalBG.classList.toggle('show')
+    
+            console.log('hello')
+            
+        })
+    
+        modals.forEach(modal => {
+            project.addEventListener('click', function(e){
+                if (project.dataset.modal == modal.dataset.modal) {
+                    modal.classList.toggle('show')
+                  }
+            })
+        })
     })
-})
-
-
-
-closeLinks.forEach(close => {
-    close.addEventListener('click', function(event) {
-        modal.classList.remove('show')
+    
+    
+    
+    closeLinks.forEach(close => {
+        close.addEventListener('click', function(event) {
+            openModal.classList.remove('show')
+            modalBG.classList.remove('show')
+        })
+    })
+    
+    modalBG.addEventListener('click', function(event) {
+        openModal.classList.remove('show')
         modalBG.classList.remove('show')
     })
-})
 
-modalBG.addEventListener('click', function(event) {
-    modal.classList.remove('show')
-    modalBG.classList.remove('show')
-})
+}
+
+
